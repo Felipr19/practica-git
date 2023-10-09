36277dcb86bec14624e178f41d4e7ccdb39845ae
Felipr19 <142014516+Felipr19@users.noreply.github.com>
2023-10-09 07:44:44 -0500

    siu

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+
+import sys
 from juego import *
 
-j = juego(MazoEspañol())
+j = Juego(MazoEspanol())
 j.iniciar_juego()
 j.jugar()
 j.valorar_juego()
+
+pygame.init()
+
+width, height = 800, 600
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("21 juego")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+
+
+    pygame.display.flip()
--- mazo.py
+++ mazo.py
@@ -1,27 +1,41 @@
 from carta import *
 from random import shuffle
-class Mazo: 
-    def __init__(self, jugador = False) -> None:
+
+class Mazo:
+    def __init__(self, jugador = False):
         if jugador:
-            self.cartas=[]
+            self.cartas = []
         else:
-            self.cartas =[ Carta(v,p) for v in [str(x) for x in range(2,11)] +['A','J','K','Q'] for p in ['Diamantes',"Corazones","Picas","Treboles"] ]
+            self.cartas = [Carta(v, p) for v in [str(x) for x in range(2,11)] + ['A', 'J', 'Q', 'K'] 
+                           for p in self.pintas] 
             shuffle(self.cartas)
 
     def mostrar_cartas(self):
         for c in self.cartas:
             print(c.mostrar_carta())
-    
-    def obtener_valor_maso(self):
-        valor = 0 
+
+    def obtener_valor_mazo(self):
+        valor = 0
         con_as = False
         for c in self.cartas:
             valor += c.obtener_valor()
-            if c.valor == "A":
+            if c.valor == 'A':
                 con_as = True
-        if con_as and valor < 11:
+        if con_as and valor <= 11:
             valor += 10
         return valor
     
     def entregar_carta(self):
         return self.cartas.pop(0)
+
+class MazoFrances(Mazo):
+    def __init__(self, jugador=False):
+        self.pintas = ['Diamantes', 'Corazones', 'Picas', 'Treboles']
+        super().__init__(jugador)
+
+
+class MazoEspanol(Mazo):
+    def __init__(self, jugador=False):
+        self.pintas = ['Bastos', 'Espadas', 'Monedas', 'Copas']
+        super().__init__(jugador)
+
